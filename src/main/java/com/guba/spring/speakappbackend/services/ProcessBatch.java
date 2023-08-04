f747df37c027ef443ad5f0e3d80f9c89a504b16d
guidobarra <guidobarra18@gmail.com>
2023-08-04 20:30:13 -0300

    add ProcessBatch

--- src/main/java/com/guba/spring/speakappbackend/services/ProcessBatch.java
+++ src/main/java/com/guba/spring/speakappbackend/services/ProcessBatch.java
@@ -1,15 +1,22 @@
 package com.guba.spring.speakappbackend.services;
 
+import com.guba.spring.speakappbackend.clients.ClientWhisperApiCustom;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.concurrent.TimeUnit;
 
-@Component
+@Service
+@Slf4j
+@AllArgsConstructor
 public class ProcessBatch {
 
-    @Scheduled(initialDelay = 0, fixedDelayString = "2", timeUnit = TimeUnit.MINUTES)
-    void callApiWhisper() {
+    private final ClientWhisperApiCustom clientWhisperApiCustom;
 
+    @Scheduled(initialDelay = 0, fixedDelay = 1, timeUnit = TimeUnit.MINUTES)
+    void callApiWhisper() {
+        //this.clientWhisperApiCustom.getTranscription();
     }
 }
