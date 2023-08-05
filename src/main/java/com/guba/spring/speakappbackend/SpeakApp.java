bca11fe739e130cc4f2fc5e6ba3b37c847438ce3
guidobarra <guidobarra18@gmail.com>
2023-08-04 22:18:18 -0300

    remove

--- src/main/java/com/guba/spring/speakappbackend/SpeakApp.java
+++ src/main/java/com/guba/spring/speakappbackend/SpeakApp.java
@@ -3,10 +3,8 @@ package com.guba.spring.speakappbackend;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 @Slf4j
 public class SpeakApp {
 
