3e5d61a58b22d795b5fb0d23a6498bebff3c520b
guidobarra <guidobarra18@gmail.com>
2023-09-23 19:15:09 -0300

    add parameter idImage

--- src/main/java/com/guba/spring/speakappbackend/web/schemas/ImageDTO.java
+++ src/main/java/com/guba/spring/speakappbackend/web/schemas/ImageDTO.java
@@ -11,11 +11,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ImageDTO {
+    private Long idImage;
     private String name;
     private String imageData;
     private String dividedName;
 
     public ImageDTO(Image image) {
+        this.idImage = image.getIdImage();
         this.name = image.getName();
         this.imageData = image.getImageData();
         this.dividedName = image.getDividedName();
