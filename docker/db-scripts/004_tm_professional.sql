a3261bfdd81460fe6b18e074661d868b66b191e9
Apolo-Accattoli <103388286+Apolo-Accattoli@users.noreply.github.com>
2023-11-04 11:19:12 -0300

    Comment 004_tm_professional.sql

--- docker/db-scripts/004_tm_professional.sql
+++ docker/db-scripts/004_tm_professional.sql
@@ -1,4 +1,5 @@
-insert into public.tm_professional (id_role, code, username, email, password, first_name, last_name, age, gender, image_data, created_at, updated_at)
+/* insert into public.tm_professional (id_role, code, username, email, password, first_name, last_name, age, gender, image_data, created_at, updated_at)
 values  (2, 'EF58F71E', 'camiPro', 'camiPro@speakapp.com', '$2a$10$EkOHuGaDtQoqouDIS.PBZOSrref6WvCHPiBS8blDyhN2EfO9HYHY.', 'camiPro', 'Paciente', 45, null, null, '2023-10-24 00:14:01.459927', '2023-10-24 00:14:01.459927'),
         (2, 'B0656457', 'luanaPro', 'luanaPro@speakapp.com', '$2a$10$X0.O9119dSb14DQ7PCKO1OPdIgm8EIqgleNZjZ2y1HSXae7wzAKJ6', 'LuanaPro', 'Profesional', 32, null, null, '2023-10-24 00:08:14.516085', '2023-10-24 00:08:14.516085'),
         (2, '4E11B9BA', 'germanPro', 'germanPro@speakapp.com', '$2a$10$/AEPlJgKScS9cXb2cXx7p.T4iyb.HARFi5IL6Ga1ZPGkGMHrON0nS', 'germanPro', 'Profesional', 27, null, null, '2023-10-24 00:14:27.186241', '2023-10-24 00:14:27.186241');
+*/
